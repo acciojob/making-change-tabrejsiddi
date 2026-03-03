091db53f8a939d008b42e4aaa376c1e876fe4c54
tabrejsiddi <139753435+tabrejsiddi@users.noreply.github.com>
2026-03-03 08:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
-  // your name here
+  let quarters = Math.floor(c / 25);
+    let remaining = c % 25;
+    
+    let dimes = Math.floor(remaining / 10);
+    remaining = remaining % 10;
+    
+    let nickels = Math.floor(remaining / 5);
+    remaining = remaining % 5;
+    
+    let pennies = remaining;  
+    
+    return {
+        q: quarters,
+        d: dimes,
+        n: nickels,
+        p: pennies
+    };
 };
 
 // Do not the change the code below
